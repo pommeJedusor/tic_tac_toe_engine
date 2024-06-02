7b595ee699ab977f49c38d7086721d003d082e18
pomme <118850425+pommeJedusor@users.noreply.github.com>
2024-06-02 14:40:30 +0200

    setter

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
+function is_valid_2dArray(arr){
+  if (!Array.isArray(arr))return false;
+  if (arr.length != 3)return false;
+  return arr.filter((row)=>{
+    if (row.length != 3)return false;
+    return row.filter((square)=>[0, 1, 2].includes(square)).length == 3;
+  }).length == 3;
+}
+
+function is_valid_1dArray(arr){
+  if (!Array.isArray(arr))return false;
+  if (arr.length != 9)return false;
+  return arr.filter((square) => [0, 1, 2].includes(square)).length == 9;
+}
+
 class Oxo{
-  p1 = 0;
-  p2 = 0;
+  _p1 = 0;
+  _p2 = 0;
+  _player_turn;
   constructor(board){
     this.setBoard(board);
   }
 
   setBoard(board){
-    if (typeof board == "string" && /[0-2]{9}/.test(board)){
-      for (let i=0;i<board.length;i++){
-        const square = board[i];
-        if (square == "1"){
-          this.p1 |= 1 << i;
-        }else if (square == "2"){
-          this.p2 |= 1 << i;
-        }
-      }
+    let get_square;
+    if (!board){
+      get_square = (board, index)=>0;
+    }
+    else if (typeof board == "string" && /[0-2]{9}/.test(board)){
+      get_square = (board, index)=>board[index];
+    }
+    else if (is_valid_1dArray(board)){
+      get_square = (board, index)=>board[index];
+    }
+    else if (is_valid_2dArray(board)){
+      get_square = (board, index)=>board[Math.floor(index/3)][index%3];
     }
     else {
       throw "board format is not valid";
+    }
+
+    this._player_turn = 1;
+    for (let i=0;i<9;i++){
+      const square = get_square(board, i);
+      if (square == "1"){
+        this._p1 |= 1 << i;
+        this._player_turn = this._player_turn % 2 + 1 ;
+      }else if (square == "2"){
+        this._p2 |= 1 << i;
+        this._player_turn = this._player_turn % 2 + 1 ;
+      }
     }
   }
 
   getStringBoard(){
     let string_board = "";
     for (let i=0;i<9;i++){
-      if (this.p1 & 1 << i)string_board += "1";
-      else if (this.p2 & 1 << i)string_board += "2";
+      if (this._p1 & 1 << i)string_board += "1";
+      else if (this._p2 & 1 << i)string_board += "2";
       else string_board += "0";
     }
     return string_board;
@@ -34,8 +65,8 @@ class Oxo{
   get1DArray(){
     const board = [];
     for (let i=0;i<9;i++){
-      if (this.p1 & 1 << i)board.push(1);
-      else if (this.p2 & 1 << i)board.push(2);
+      if (this._p1 & 1 << i)board.push(1);
+      else if (this._p2 & 1 << i)board.push(2);
       else board.push(0);
     }
     return board;
@@ -47,8 +78,8 @@ class Oxo{
       board.push([]);
       for (let j=0;j<3;j++){
         const index = i*3 +j;
-        if (this.p1 & 1 << index)board.at(-1).push(1);
-        else if (this.p2 & 1 << index)board.at(-1).push(2);
+        if (this._p1 & 1 << index)board.at(-1).push(1);
+        else if (this._p2 & 1 << index)board.at(-1).push(2);
         else board.at(-1).push(0);
       }
     }
